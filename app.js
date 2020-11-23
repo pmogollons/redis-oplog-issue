12511d6b7859ebb83a3926ac04e6f24031939c3e
Paulo Mogollón <pmogollon@propiedata.com>
2020-11-23 13:41:03 -0500

    Reproduction for issue ramezrafla/redis-oplog#6

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 import { Mongo } from 'meteor/mongo';
 
 
-const Assemblies = new Mongo.Collection('assemblies');
+export const Assemblies = new Mongo.Collection('assemblies');
 
 
 Assemblies.startCaching();
+
+Assemblies.before.insert(function beforeInsert(userId, doc) {
+  doc.object = { field1: 'value 1', field2: 'value 2' };
+});
+
+
+Meteor.methods({
+  async test() {
+    const _id = Assemblies.insert({
+      field0: 'value 0'
+    });
+
+    return {
+      cache: Assemblies.getCache(_id),
+      findOne: Assemblies.findOne(_id),
+      docInDB: await Assemblies.rawCollection().findOne({ _id }, { projection: { _id: true, field0: true, object: true } })
+    }
+  }
+});
